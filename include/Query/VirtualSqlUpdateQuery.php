8eebe14f9a394f1e5872bcc0a62785db4cbe88bd
zethika <zethika594@gmail.com>
2022-04-30 13:27:31 +0200

    Feature: Added update statement support

--- include/Query/VirtualSqlUpdateQuery.php
+++ include/Query/VirtualSqlUpdateQuery.php
@@ -2,12 +2,64 @@
 
 namespace VirtualSql\Query;
 
+use VirtualSql\Definition\VirtualSqlColumn;
+use VirtualSql\Definition\VirtualSqlTable;
 use VirtualSql\Query\Partials\WhereAbleSqlQuery;
+use VirtualSql\SqlBuilder\VirtualSqlUpdateBuilder;
 
 class VirtualSqlUpdateQuery extends WhereAbleSqlQuery
 {
+	/**
+	 * @var VirtualSqlColumn[]
+	 */
+	protected array $columns = [];
 
-	// Values headers
-	// Values rows
-	// Where
+	/**
+	 * @var array
+	 */
+	protected array $values = [];
+
+	/**
+	 * @param VirtualSqlTable $table
+	 * @param array $config
+	 */
+	public function __construct(VirtualSqlTable $table, array $config)
+	{
+		$this->builder = new VirtualSqlUpdateBuilder($this);
+		$this->columns = isset($config['columns']) && is_array($config['columns']) ? array_values(array_filter($config['columns'],fn($column) => $column instanceof VirtualSqlColumn)) : [];
+		$this->values = isset($config['values']) && is_array($config['values']) ? $config['values'] : [];
+		parent::__construct($table,$config);
+	}
+
+	/**
+	 * @return VirtualSqlColumn[]
+	 */
+	public function getColumns(): array
+	{
+		return $this->columns;
+	}
+
+	/**
+	 * @param VirtualSqlColumn[] $columns
+	 */
+	public function setColumns(array $columns): void
+	{
+		$this->columns = $columns;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getValues(): array
+	{
+		return $this->values;
+	}
+
+	/**
+	 * @param array $values
+	 */
+	public function setValues(array $values): void
+	{
+		$this->values = $values;
+	}
 }
--- include/SqlBuilder/VirtualSqlUpdateBuilder.php
+++ include/SqlBuilder/VirtualSqlUpdateBuilder.php
@@ -2,6 +2,7 @@
 
 namespace VirtualSql\SqlBuilder;
 
+use VirtualSql\Definition\VirtualSqlColumn;
 use VirtualSql\Exceptions\InvalidQueryPartException;
 use VirtualSql\Query\VirtualSqlQuery;
 use VirtualSql\Query\VirtualSqlUpdateQuery;
@@ -38,6 +39,20 @@ class VirtualSqlUpdateBuilder extends WhereAbleSqlBuilder
 	{
 		$string = 'UPDATE '.$this->getAliasedTableName($this->getQuery()->getBaseTable());
 
+		$values = $this->getQuery()->getValues();
+		$parts = array_map(fn(VirtualSqlColumn $column) => $column->getColumn().' = '.$this->parseAddValue($column, $values[$column->getColumn()] ?? null), $this->getQuery()->getColumns());
+		$string .= ' SET '.implode(', ',$parts);
+
+		$parts = [
+			$this->buildJoinString($this->getQuery()->getJoins()),
+			$this->buildWhereString($this->getQuery()->getWhere()),
+		];
+
+		foreach ($parts as $part)
+		{
+			if($part !== null)
+				$string .= ' '.$part;
+		}
 
 		return $string;
 	}
