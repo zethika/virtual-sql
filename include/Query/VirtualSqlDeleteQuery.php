83f7b86ca88487bd3c0b75e6ef40e3183d0b1391
zethika <zethika594@gmail.com>
2022-04-30 13:30:37 +0200

    Feature: Added support for delete query

--- include/Query/VirtualSqlDeleteQuery.php
+++ include/Query/VirtualSqlDeleteQuery.php
@@ -2,10 +2,19 @@
 
 namespace VirtualSql\Query;
 
+use VirtualSql\Definition\VirtualSqlTable;
 use VirtualSql\Query\Partials\LimitAbleSqlQuery;
+use VirtualSql\SqlBuilder\VirtualSqlDeleteBuilder;
 
 class VirtualSqlDeleteQuery extends LimitAbleSqlQuery
 {
-	// Where
-	// limit
+	/**
+	 * @param VirtualSqlTable $table
+	 * @param array $config
+	 */
+	public function __construct(VirtualSqlTable $table, array $config)
+	{
+		$this->builder = new VirtualSqlDeleteBuilder($this);
+		parent::__construct($table, $config);
+	}
 }
