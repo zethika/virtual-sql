5904bee19d72b7272d5cf66540e36aaa13fcf731
Kenneth <kenneth@esmark.dk>
2023-06-09 15:35:11 +0200

    feature: Added additional column types

--- include/VirtualSqlConstant.php
+++ include/VirtualSqlConstant.php
@@ -45,19 +45,32 @@ class VirtualSqlConstant
     const COLUMN_TYPE_CHAR = 'char';
     const COLUMN_TYPE_LONGTEXT = 'longtext';
     const COLUMN_TYPE_MEDIUMTEXT = 'mediumtext';
+    const COLUMN_TYPE_TINYTEXT = 'tinytext';
     const COLUMN_TYPE_TEXT = 'text';
-    const COLUMN_TYPE_BLOB = 'blob';
     const COLUMN_TYPE_JSON = 'json';
+    const COLUMN_TYPE_BIT = 'bit';
 
     const COLUMN_TEXT_TYPES = [self::COLUMN_TYPE_VARCHAR, self::COLUMN_TYPE_CHAR];
 
     /**
+     * BLOBS
+     */
+    const COLUMN_TYPE_TINYBLOB = 'tinyblob';
+    const COLUMN_TYPE_MEDIUMBLOB = 'mediumblob';
+    const COLUMN_TYPE_BLOB = 'blob';
+    const COLUMN_TYPE_LONGBLOB = 'longblob';
+
+
+    /**
      * NUMBER
      */
-    const COLUMN_TYPE_INT = 'int';
     const COLUMN_TYPE_TINYINT = 'tinyint';
+    const COLUMN_TYPE_SMALLINT = 'smallint';
+    const COLUMN_TYPE_MEDIUMINT = 'mediumint';
+    const COLUMN_TYPE_INT = 'int';
     const COLUMN_TYPE_BIGINT = 'bigint';
     const COLUMN_TYPE_DECIMAL = 'decimal';
+    const COLUMN_TYPE_DOUBLE = 'double';
     const COLUMN_TYPE_FLOAT = 'float';
 
     const COLUMN_NUMBER_TYPES = [self::COLUMN_TYPE_INT, self::COLUMN_TYPE_TINYINT, self::COLUMN_TYPE_BIGINT, self::COLUMN_TYPE_DECIMAL, self::COLUMN_TYPE_FLOAT];
@@ -65,8 +78,32 @@ class VirtualSqlConstant
     /**
      * DATES
      */
+    const COLUMN_TYPE_DATE = 'date';
     const COLUMN_TYPE_DATETIME = 'datetime';
     const COLUMN_TYPE_TIMESTAMP = 'timestamp';
+    const COLUMN_TYPE_TIME = 'time';
+    const COLUMN_TYPE_YEAR = 'year';
+
+    /**
+     * GEOMETRY
+     */
+    const COLUMN_TYPE_GEOMETRY = 'geometry';
+    const COLUMN_TYPE_POINT = 'point';
+    const COLUMN_TYPE_LINESTRING = 'linestring';
+    const COLUMN_TYPE_POLYGON = 'polygon';
+    const COLUMN_TYPE_MULTIPOINT = 'multipoint';
+    const COLUMN_TYPE_MULTILINESTRING = 'multilinestring';
+    const COLUMN_TYPE_MULTIPOLYGON = 'multipolygon';
+    const COLUMN_TYPE_GEOMETRYCOLLECTION = 'geometrycollection';
+
+    /**
+     * BINARY
+     */
+    const COLUMN_TYPE_BINARY = 'binary';
+    const COLUMN_TYPE_VARBINARY = 'varbinary';
+
+    const COLUMN_TYPE_ENUM = 'enum';
+    const COLUMN_TYPE_SET = 'set';
 
     /**
      * DEFAULTS
